6ff1a20daf983006a5ffabd6369862db8dc0b485
xuepengqiang <506321815@qq.com>
2020-04-26 20:56:10 +0800

    add m

--- 09_mul_thread_random.cpp
+++ 09_mul_thread_random.cpp
@@ -4,6 +4,11 @@
 #include <ctime>
 #include <cstdlib>
 
+'''
+运行命令
+g++ -std=c++11 sample.cpp -lpthread
+'''
+
 void subthread_function()
 {
 	srand(time(0));
@@ -25,3 +30,35 @@ int main(int argc, char const *argv[])
 	threadObj2.join();
 	return 0;
 }
+
+
+// static void subthread_function(int cc)
+// {
+// 	srand(time(0));
+// 	for (int i = 0; i < 10; ++i)
+// 	{
+// 		int random_seconds = rand()%cc;
+// 		/* code */
+// 		std::cout << cc << " :ID: = " << std::this_thread::get_id() << "  :  " <<  random_seconds << std::endl;
+// 		sleep(random_seconds);
+// 	}
+// }
+
+// int main(int argc, char const *argv[])
+// {
+// 	std::vector<std::thread> vec;
+
+
+// 	for (int i = 2; i < 5; ++i)
+// 	{
+// 		std::thread threadObj(subthread_function, i);
+
+// 		vec.emplace_back(std::move(threadObj));
+// 	}
+
+// 	for (auto &a : vec)
+// 	{
+// 		a.join();
+// 	}
+// 	return 0;
+// }
