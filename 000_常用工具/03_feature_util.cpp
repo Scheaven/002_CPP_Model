5b4ef8b22ede0fe44733bd230406fcebdd5e1d07
Scheaven <506321815@qq.com>
2020-09-12 16:50:59 +0800

    update distance

---
+++ 000_常用工具/03_feature_util.cpp
@@ -0,0 +1,36 @@
+
+double feature_distance(FEATURE feat1, FEATURE feat2)
+{
+
+    //将float格式的特征转化为tensor格式的特征
+ 
+   double FeatA, FeatB, score;
+
+   for (int i = 0; i < 128; i++)
+
+    {
+   
+        FeatA += double(feat1[i])*double(feat1[i]);
+
+        FeatB += double(feat2[i])*double(feat2[i]);
+
+        score += double(feat1[i])*double(feat2[i]);
+
+    }
+
+
+
+    
+    FeatA = sqrt(FeatA);
+    
+   FeatB = sqrt(FeatB);
+
+
+   score = score / (FeatA * FeatB);
+   if (score < 0) {
+        score = 0;
+    }
+ 
+   printf("--评分---%f--",score);
+    return score;
+}
